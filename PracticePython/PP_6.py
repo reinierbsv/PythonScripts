b678c4bb67984275fc1b9fc6aea9e26c5df26468
reinierbsv <reinierbsv@gmail.com>
2017-07-17 19:18:00 -0400

    Update PP_6.py

--- PracticePython/PP_6.py
+++ PracticePython/PP_6.py
@@ -2,13 +2,16 @@
 Ask the user for a string and print out whether this string is a palindrome or not.
 (A palindrome is a string that reads the same forwards and backwards.)
 '''
-a = raw_input("Enter a word to verify if it is a palindrome or not: ")
-if len(a) % 2 != 0:
+a = input("Enter a word to verify if it is a palindrome or not: ")
+if len(a) % 2 == 0:
+    print(a, "is not a palindrome")
+else:
     i = 0
-    for i in range((len(a) - 1) / 2):
-        if a.__getitem__(i) != a.__getitem__((len(a) - 1) - i):
-            print a, "is not a palindrome"
+    while i < int((len(a) - 1) / 2):
+        if a.__getitem__(i) == a.__getitem__((len(a) - 1) - i):
+            i = i + 1
+        else:
+            print(a, "is not a palindrome")
             break
-        elif a.__getitem__(i) == a.__getitem__((len(a) - 1) - i):
-            pass
-    print a, "is a palindrome"
+    if i == int((len(a) - 1) / 2):
+            print(a, "is a palindrome")
