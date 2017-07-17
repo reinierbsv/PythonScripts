253d2b58840096fd5b6b44fc2fb100a7a2900fe6
reinierbsv <reinierbsv@gmail.com>
2017-07-17 01:21:27 -0400

    Update PP_4.py

--- PracticePython/PP_4.py
+++ PracticePython/PP_4.py
@@ -1,5 +1,8 @@
-
+'''
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+'''
 num = input("Enter the number for witch you want to find the divisors: ")
+num = int(num)
 A = []
 for x in range(2, num):
     if num % x == 0:
