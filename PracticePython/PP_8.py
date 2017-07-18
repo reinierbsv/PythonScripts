c3ef610cc507cf996a7f170f67ebfcb9e7ccdbc5
reinierbsv <reinierbsv@gmail.com>
2017-07-18 00:40:54 -0400

    Add files via upload

---
+++ PracticePython/PP_8.py
@@ -0,0 +1,26 @@
+'''
+Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them,
+print out a message of congratulations to the winner, and ask if the players want to start a new game)
+
+Remember the rules:
+Rock beats scissors
+Scissors beats paper
+Paper beats rock
+'''
+ply_1 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
+ply_2 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
+if ply_1 == "P" and ply_2 == "R":
+    print("Player 1 wins")
+elif ply_1 == "P" and ply_2 == "S":
+    print("Player 2 wins")
+elif ply_1 == "R" and ply_2 == "P":
+    print("Player 2 wins")
+elif ply_1 == "R" and ply_2 == "S":
+    print("Player 1 wins")
+elif ply_1 == "S" and ply_2 == "P":
+    print("Player 1 wins")
+elif ply_1 == "S" and ply_2 == "R":
+    print("Player 2 wins")
+else:
+    print("Null game")
+    ans = input("Would you like to play again")
