2caf9b66b6a9377368bf2408710283e15c0e3ac9
reinierbsv <reinierbsv@gmail.com>
2017-07-18 00:49:31 -0400

    Update PP_8.py

--- PracticePython/PP_8.py
+++ PracticePython/PP_8.py
@@ -7,20 +7,56 @@ Rock beats scissors
 Scissors beats paper
 Paper beats rock
 '''
-ply_1 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
-ply_2 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
-if ply_1 == "P" and ply_2 == "R":
-    print("Player 1 wins")
-elif ply_1 == "P" and ply_2 == "S":
-    print("Player 2 wins")
-elif ply_1 == "R" and ply_2 == "P":
-    print("Player 2 wins")
-elif ply_1 == "R" and ply_2 == "S":
-    print("Player 1 wins")
-elif ply_1 == "S" and ply_2 == "P":
-    print("Player 1 wins")
-elif ply_1 == "S" and ply_2 == "R":
-    print("Player 2 wins")
-else:
-    print("Null game")
-    ans = input("Would you like to play again")
+i = 1
+while i == 1:
+    ply_1 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
+    ply_2 = input("Enter your choice: P:Paper or S:Scissors or R:Rock")
+    if ply_1 == "P" and ply_2 == "R":
+        print("Player 1 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    elif ply_1 == "P" and ply_2 == "S":
+        print("Player 2 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    elif ply_1 == "R" and ply_2 == "P":
+        print("Player 2 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    elif ply_1 == "R" and ply_2 == "S":
+        print("Player 1 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    elif ply_1 == "S" and ply_2 == "P":
+        print("Player 1 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    elif ply_1 == "S" and ply_2 == "R":
+        print("Player 2 wins")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
+    else:
+        print("Null game")
+        ans = input("Would you like to play again, Y:Yes or N:No")
+        if ans == "Y":
+            i = 1
+        else:
+            i = 2
