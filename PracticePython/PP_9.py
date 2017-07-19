2bcfc65019b6d83de3eb048633ed5a06a452391f
reinierbsv <reinierbsv@gmail.com>
2017-07-19 12:51:45 -0400

    Update PP_9.py

--- PracticePython/PP_9.py
+++ PracticePython/PP_9.py
@@ -1,4 +1,4 @@
-'''
+"""
 Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then
 tell them whether they guessed too low, too high, or exactly right. (Hint: remember to use the user
 input lessons from the very first exercise)
@@ -6,8 +6,10 @@ input lessons from the very first exercise)
 Extras:
 Keep the game going until the user types exit
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
-'''
+"""
 import random
+
+
 x = random.randint(1, 11)
 print("On this game you have to guess which number between 1 and 9 I choose before")
 print("If you want to end the game type 'exit'")
