ec3d674e09cd6e9bcc6ccdff3e35b854997e4f5a
reinierbsv <reinierbsv@gmail.com>
2017-07-19 12:58:47 -0400

    Update and rename PP_1 to PP_1.py

--- PracticePython/PP_1.py
+++ PracticePython/PP_1.py
@@ -1,4 +1,4 @@
-'''
+"""
 Create a program that asks the user to enter their name and their age. Print out a message addressed to them
 that tells them the year that they will turn 100 years old.
 
@@ -7,8 +7,10 @@ Add on to the previous program by asking the user for another number and printin
 previous message. (Hint: order of operations exists in Python)
 Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as
 pressing the ENTER button)
-'''
+"""
 import datetime
+
+
 name = input('Enter your name: ')
 age = input('Enter your age: ')
 date = datetime.datetime.now().strftime("%Y")
