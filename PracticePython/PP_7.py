a03c8842706db829172d2315502a7cfecc62a028
reinierbsv <reinierbsv@gmail.com>
2017-07-19 12:53:00 -0400

    Update PP_7.py

--- PracticePython/PP_7.py
+++ PracticePython/PP_7.py
@@ -1,6 +1,6 @@
-'''
+"""
 Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
 Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
-'''
+"""
 A = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 B = [a for a in A if a % 2 == 0]; print(B)
