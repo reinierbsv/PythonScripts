24335cc633e7fbf16318cb26d8809357f27f29da
reinierbsv <reinierbsv@gmail.com>
2017-07-19 12:56:34 -0400

    Update PP_2.py

--- PracticePython/PP_2.py
+++ PracticePython/PP_2.py
@@ -1,4 +1,4 @@
-'''
+"""
 Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
 Hint: how does an even / odd number react differently when divided by 2?
 
@@ -6,7 +6,7 @@ Extras:
 If the number is a multiple of 4, print out a different message.
 Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
 If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
-'''
+"""
 number = input("Enter a number: ")
 if int(number) % 4 == 0:
     print("The number is multiple of 4")
