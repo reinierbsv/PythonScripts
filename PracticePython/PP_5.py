ac963f33b9ef849ca5405b796a13b6898bbe3dbb
reinierbsv <reinierbsv@gmail.com>
2017-07-19 12:54:18 -0400

    Update PP_5.py

--- PracticePython/PP_5.py
+++ PracticePython/PP_5.py
@@ -1,4 +1,4 @@
-'''
+"""
 Take two lists, say for example these two:
   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -8,8 +8,10 @@ and write a program that returns a list that contains only the elements that are
 Extras:
 Randomly generate two lists to test this
 Write this in one line of Python
-'''
+"""
 import random as rd
+
+
 def listInter(A, B):
     C = []
     D = set(A)
