6d8f55920eab8bc3a1275dc543c222376c4a47ed
reinierbsv <reinierbsv@gmail.com>
2017-08-17 00:25:44 -0400

    Update PP_20.py

--- PracticePython/PP_20.py
+++ PracticePython/PP_20.py
@@ -2,41 +2,49 @@
 Write a function that takes an ordered list of numbers (a list where the elements are in order
 from smallest to largest) and another number. The function decides whether or not the given number
 is inside the list and returns (then prints) an appropriate boolean.
-
 Extras:
 Use binary search.
 """
 
-def elmsearch (a, num):
-    if num in a:
-        return "True"
+
+def elmsearch(a, num):
+    i = 0
+    while i <= len(a) - 1:
+        if a[i] == num:
+            return "True"
+        else:
+            i += 1
     else:
         return "False"
 
-def binarySearch(a, nmm):
+
+def binarysearch(a, num):
     key = False
-    pointer = int(len(a)/2) - 1
-    if num == a[0]:
+    if num == a[0] or num == a[len(a) - 1]:
         key = True
     else:
+        pmin = 1
+        pmax = len(a) - 2
+        p = int((pmax - pmin) / 2)
         while key is False:
-            if num == a[pointer]:
-                key = True
+            if int((pmax - pmin) / 2) == 0:
                 break
-            elif num > a[pointer]:
-                pointer = pointer + int(pointer/2)
-                print pointer
-                if pointer == len(a):
-                    break
+            elif num == a[p]:
+                key = True
+            elif num > a[p]:
+                pmin = p + 1
+                p = pmax - int((pmax - pmin) / 2)
+                if num == a[p]:
+                    key = True
             else:
-                pointer = pointer - int(pointer/2)
-                print pointer
-                if pointer == 1:
-                    break
+                pmax = p - 1
+                p = pmin + int((pmax - pmin) / 2)
+                if num == a[p]:
+                    key = True
     return key
 
 a = [1, 3, 8, 15, 68, 78, 99, 124, 147]
-num = 82
+num = 100
 
-print elmsearch(a, num)
-print binarySearch(a, num)
+print(elmsearch(a, num))
+print(binarysearch(a, num))
